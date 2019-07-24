b3fdb0e1c6d2f672a7697205faff9eb18d867023
Die4Ever <die4ever2005@gmail.com>
2019-07-24 03:16:44 -0500

    small tweaks

--- rotate_logs.sh
+++ rotate_logs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-rm rsync_log.txt.old ; mv rsync_log.txt rsync_log.txt.old ; touch rsync_log.txt
+
+rm rsync_log.txt.old
+mv rsync_log.txt rsync_log.txt.old
+touch rsync_log.txt
